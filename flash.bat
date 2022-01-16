ccde588e5309e21eb23a314e536b07e8ec3c5c4b
dan-mannion <danjohnmannion@gmail.com>
2022-01-16 13:50:14 +0000

    Modified flash to now accept com port via command line argument

--- flash.bat
+++ flash.bat
@@ -1,2 +1,3 @@
 msp430-elf-objcopy -O ihex %1 %~dpn1.hex -v    
-MSP430Flasher -n msp430g2xx3 -i USB -z [vcc] -w %~dpn1.hex
+MSP430Flasher -n msp430g2xx3 -i %2 -z [vcc] -w %~dpn1.hex
+start putty -load tiles -serial %2 
