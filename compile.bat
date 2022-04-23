fcdc2a75a7baaa738ba3c2101318799c7696b5d4
dan-mannion <danjohnmannion@gmail.com>
2022-04-23 21:18:48 +0100

    Adds latest .o files

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-msp430-elf-gcc -mmcu=msp430g2553 -L C:\ti\msp430-gcc\include -I C:\ti\msp430-gcc\include obj\system.o obj\watchdog.o obj\gpio.o obj\uart.o %1 -o %~dpn1.o
+msp430-elf-gcc -mmcu=msp430g2553 -L C:\ti\msp430-gcc\include -I C:\ti\msp430-gcc\include obj\system.o obj\clocks.o obj\watchdog.o obj\gpio.o obj\uart.o obj\timer.o %1 -o %~dpn1.o
