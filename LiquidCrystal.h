4bfb19803e3fd95bd13f4eb45acfacebacf25c92
dan-mannion <danjohnmannion@gmail.com>
2022-06-01 17:30:39 +0100

    Moves private functions from header to c file

--- LiquidCrystal.h
+++ LiquidCrystal.h
@@ -67,29 +67,6 @@
   void autoscroll();
   void noAutoscroll();
 
-  void setRowOffsets(int row1, int row2, int row3, int row4);
-  void createChar(uint8_t, uint8_t[]);
-  void setCursor(uint8_t, uint8_t); 
-  virtual size_t write(uint8_t);
-  void command(uint8_t);
-  
-  void send(uint8_t, uint8_t);
-  void write4bits(uint8_t);
-  void write8bits(uint8_t);
-  void pulseEnable();
 
-  uint8_t _rs_pin; // LOW: command. HIGH: character.
-  uint8_t _rw_pin; // LOW: write to LCD. HIGH: read from LCD.
-  uint8_t _enable_pin; // activated by a HIGH pulse.
-  uint8_t _data_pins[8];
-
-  uint8_t _displayfunction;
-  uint8_t _displaycontrol;
-  uint8_t _displaymode;
-
-  uint8_t _initialized;
-
-  uint8_t _numlines;
-  uint8_t _row_offsets[4];
 
 #endif
