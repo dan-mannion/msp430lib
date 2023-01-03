0f089d9172f983625d6e95c7a4e2425b9b0034c0
dan-mannion <danjohnmannion@gmail.com>
2023-01-03 10:48:10 +0000

    Initial script to test screen works. This works fine with screen. Now I need to wrap this up in functions and develop a vector graphics library

--- graphics_demo.c
+++ graphics_demo.c
@@ -4,7 +4,7 @@ struct LCD{
 	char control_port;
 	char chip_select_pin;
 	char write_enable_pin;
-	char data_command_select;
+	char data_command_select_pin;
 	char data_bus_port;
 };
 
@@ -12,59 +12,109 @@ struct LCD lcdInit(){
 	//Initialises and returns a lcd struct
 	struct LCD lcd; 
 	lcd.control_port = PORT1;
-	lcd.chip_select_pin = 8;
-	lcd.write_enable_pin = 7;
-	lcd.data_command_select = 6;
+	lcd.chip_select_pin = 5;
+	lcd.write_enable_pin = 4;
+	lcd.data_command_select_pin = 3;
 	lcd.data_bus_port = PORT2;	
+	
+	//Initilise gpio pins. 
+	setPinMode(lcd.control_port, lcd.chip_select_pin, OUTPUT);
+	writePinOutput(lcd.control_port, lcd.chip_select_pin, LOW);
+	setPinMode(lcd.control_port, lcd.write_enable_pin, OUTPUT);
+	writePinOutput(lcd.control_port, lcd.write_enable_pin, HIGH);
+	setPinMode(lcd.control_port, lcd.data_command_select_pin, OUTPUT);
+	writePinOutput(lcd.control_port, lcd.data_command_select_pin, HIGH);
+
+	//Initialise data pins
+	int n=0;
+	for (n=0;n<8;n++){
+		selectPinFunction(lcd.data_bus_port, n, SEL_IO);
+		setPinMode(lcd.data_bus_port, n, OUTPUT);
+	}	
+	writeCharToPortOutput(lcd.data_bus_port, 0);
+
 	return lcd;
 }
 
 void lcdWriteCommand(struct LCD *lcd, unsigned char cmd){
+	//Indicate the data to be sent is a command
+	writePinOutput(lcd->control_port, lcd->data_command_select_pin, LOW);
 	//Inidicate to chip we want to write
 	writePinOutput(lcd->control_port, lcd->write_enable_pin, LOW);
-	//Indicate the data to be sent is a command
-	writePinOutput(lcd->control_port, lcd->data_command_select, LOW);
 	//Select chip
-	writePinOutput(lcd->control_port, lcd->chip_select_pin, LOW);
+	//writePinOutput(lcd->control_port, lcd->chip_select_pin, LOW);
 	//Write to datalines
-	P2OUT = cmd;
+	writeCharToPortOutput(lcd->data_bus_port, cmd);
+	//delayMillisecondTimer1(500);
 	// INdicate data writte
 	writePinOutput(lcd->control_port, lcd->write_enable_pin, HIGH);
 	//Wait for chip to read command
-	delayMillisecondTimer1(10);
+	//delayMillisecondTimer1(1);
 	//Signal end of transmission
-	writePinOutput(lcd->control_port, lcd->chip_select_pin, HIGH);
-	
-	
-
+	//writePinOutput(lcd->control_port, lcd->chip_select_pin, HIGH);
+	writePinOutput(lcd->control_port, lcd->write_enable_pin, HIGH);
+	writePinOutput(lcd->control_port, lcd->data_command_select_pin, HIGH);
+}
+void lcdWriteData(struct LCD *lcd, unsigned char cmd){
+	//Indicate the data to be sent is a command
+	writePinOutput(lcd->control_port, lcd->data_command_select_pin, HIGH);
+	//Inidicate to chip we want to write
+	writePinOutput(lcd->control_port, lcd->write_enable_pin, LOW);
+	//Select chip
+	//writePinOutput(lcd->control_port, lcd->chip_select_pin, LOW);
+	//Write to datalines
+	writeCharToPortOutput(lcd->data_bus_port, cmd);
+	//delayMillisecondTimer1(250);
+	// INdicate data writte
+	writePinOutput(lcd->control_port, lcd->write_enable_pin, HIGH);
+	//Wait for chip to read command
+	//delayMillisecondTimer1(250);
+	//Signal end of transmission
+	//writePinOutput(lcd->control_port, lcd->chip_select_pin, HIGH);
 }
 void main(){
 	systemInit();
 
-	struct LCD lcd;
+	struct LCD lcd = lcdInit();
 
-	unsigned char cmd_DISPON = (unsigned char)0x00101001;
-	unsigned char cmd_DISPOFF = (unsigned char) 0x00101000;
+	unsigned char cmd_DISPON = (unsigned char) 0x29;
+	unsigned char cmd_DISPOFF = (unsigned char) 0x28;
+	unsigned char cmd_WRITERAM = (unsigned char) 0x2c;
+	unsigned char cmd_INVON = (unsigned char) 0x21;
+	unsigned char cmd_INVOFF = (unsigned char) 0x20;
+	unsigned char cmd_SLPOUT = (unsigned char) 0x11;
+	unsigned char cmd_COLMOD = 0x3a;
 	
-	int n;	
-	while(1){
+		//writePinOutput(lcd.control_port, lcd.chip_select_pin, HIGH);
+		//delayMillisecondTimer1(500);
+		writePinOutput(lcd.control_port, lcd.chip_select_pin, LOW);
+		delayMillisecondTimer1(500);
+		lcdWriteCommand(&lcd, cmd_SLPOUT);
 		lcdWriteCommand(&lcd, cmd_DISPON);
-		for(n=0;n<6;n++)
+		lcdWriteCommand(&lcd, cmd_COLMOD);
+		lcdWriteData(&lcd, 0x03);
+		lcdWriteCommand(&lcd, cmd_WRITERAM);
+	int n;
+	while(1){
+		/*lcdWriteCommand(&lcd, cmd_DISPON);
+		for(n=0;n<2;n++)
 			delayMillisecondTimer1(500);
 		lcdWriteCommand(&lcd, cmd_DISPOFF);
-		for(n=0;n<6;n++)
+		for(n=0;n<2;n++)
 			delayMillisecondTimer1(500);
+		*/	
+		//lcdWriteCommand(&lcd, cmd_WRITERAM);
+		for(n=0;n<0xff;n++){
+		lcdWriteData(&lcd, 0xaa);
+		lcdWriteData(&lcd, 0x11);
+		lcdWriteData(&lcd, 0xff);
+		lcdWriteData(&lcd, 0x5c);
+		lcdWriteData(&lcd, 0x31);
+		}
+		for(n=0;n<0xcc;n++){
+			lcdWriteData(&lcd, 0xff);
+		}
+	//	lcdWriteCommand(&lcd, cmd_DISPOFF);
+			
 	}
-
-	/*setPinMode(1, 0, OUTPUT);
-	setPinMode(1, 6, OUTPUT);
-	while(1){
-		writePinOutput(1,0, HIGH);
-		writePinOutput(1,6, LOW);
-		for(i=0;i<delay;i++){;}
-		writePinOutput(1,0,LOW);
-		writePinOutput(1,6,HIGH);
-		for(i=0;i<delay;i++){;}
-	}
-	*/
 }
